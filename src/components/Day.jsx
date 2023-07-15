acfe5a3cb6b622f7382dabd3120e0794f173fb4d
Paul Llanque <llanquepol@gmail.com>
2023-07-15 11:13:30 -0500

    add Day component

---
+++ src/components/Day.jsx
@@ -0,0 +1,14 @@
+const Day = ({ day, rowIndex }) => {
+  return (
+    <div className="border border-gray-200 flex flex-col">
+      <header className="flex flex-col items-center">
+        {rowIndex % 7 === 0 && (
+          <p className="text-sm mt-1">{day.format('ddd').toUpperCase()}</p>
+        )}
+        <p className="text-sm p-1 my-1 text-center">{day.format('DD')}</p>
+      </header>
+    </div>
+  );
+};
+
+export default Day;
