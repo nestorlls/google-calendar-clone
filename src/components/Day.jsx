1c29b2c299fa27736fcdd472afd894547bfde4c6
Paul Llanque <llanquepol@gmail.com>
2023-07-15 16:28:14 -0500

    update adding style for current day

--- src/components/Day.jsx
+++ src/components/Day.jsx
@@ -1,11 +1,20 @@
+import dayjs from 'dayjs';
+
 const Day = ({ day, rowIndex }) => {
+  const getCurrentDayClass = () => {
+    return day.format('DD-MM-YY') === dayjs().format('DD-MM-YY')
+      ? 'bg-blue-600 text-white rounded-full w-7 h-7'
+      : '';
+  };
   return (
     <div className="border border-gray-200 flex flex-col">
       <header className="flex flex-col items-center">
         {rowIndex % 7 === 0 && (
           <p className="text-sm mt-1">{day.format('ddd').toUpperCase()}</p>
         )}
-        <p className="text-sm p-1 my-1 text-center">{day.format('DD')}</p>
+        <p className={`text-sm p-1 my-1 text-center ${getCurrentDayClass()}`}>
+          {day.format('DD')}
+        </p>
       </header>
     </div>
   );
