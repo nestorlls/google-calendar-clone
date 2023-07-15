c37e166df232d605235297afe1a941604753c9d5
Paul Llanque <llanquepol@gmail.com>
2023-07-15 11:13:06 -0500

    add create matrix for months utils

---
+++ src/utils.js
@@ -0,0 +1,18 @@
+import dayjs from 'dayjs';
+
+export const getMonth = (month = dayjs().month()) => {
+  const year = dayjs().year();
+
+  const firstDayOfMonth = dayjs(new Date(year, month, 1)).day();
+
+  let currentMonth = 0 - firstDayOfMonth;
+
+  const daysMatrix = new Array(5).fill([]).map(() => {
+    return new Array(7).fill(null).map(() => {
+      currentMonth++;
+      return dayjs(new Date(year, month, currentMonth));
+    });
+  });
+
+  return daysMatrix;
+};
