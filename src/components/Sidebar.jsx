dc97ec6d48fe76bbc026d0ae88767bca1a4fb21c
Paul Llanque <llanquepol@gmail.com>
2023-07-15 22:38:05 -0500

    add sidebar

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,13 @@
+import { CreateEventButton } from './CreateEventButton';
+import SmallCalendar from './SmallCalendar';
+
+const Sidebar = () => {
+  return (
+    <aside className="border p-5 w-72 ">
+      <CreateEventButton />
+      <SmallCalendar />
+    </aside>
+  );
+};
+
+export default Sidebar;
