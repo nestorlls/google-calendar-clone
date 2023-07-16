7960ba71b4954832fee4965367f07cef8207efc6
Paul Llanque <llanquepol@gmail.com>
2023-07-15 22:37:37 -0500

    add global context

--- src/context/GlogalContext.jsx
+++ src/context/GlogalContext.jsx
@@ -3,6 +3,16 @@ import { createContext } from 'react';
 const GlobalContext = createContext({
   monthIndex: 0,
   setMonthIndex: (index) => {},
+  smallCalendarMonth: 0,
+  setSmallCalendarMonth: (index) => {},
+  daySelected: null,
+  setDaySelected: (day) => {},
+  showEventModal: false,
+  setShowEventModal: () => {},
+  dispatchCalEvent: ({ type, payload }) => {},
+  savedEvents: [],
+  selectedEvent: null,
+  setSelectedEvent: () => {},
 });
 
 export default GlobalContext;
