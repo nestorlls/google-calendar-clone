78e284279b56301c5d06e2bcd632d49167d025d9
Paul Llanque <llanquepol@gmail.com>
2023-07-15 22:34:03 -0500

    update day component

--- src/components/Day.jsx
+++ src/components/Day.jsx
@@ -1,21 +1,53 @@
 import dayjs from 'dayjs';
+import { useContext, useEffect, useState } from 'react';
+import GlobalContext from '../context/GlogalContext';
+import EventModal from './EventModal';
+
+const getCurrentDayClass = (day) => {
+  return day.format('DD-MM-YY') === dayjs().format('DD-MM-YY')
+    ? 'bg-blue-600 text-white rounded-full w-7 h-7'
+    : '';
+};
 
 const Day = ({ day, rowIndex }) => {
-  const getCurrentDayClass = () => {
-    return day.format('DD-MM-YY') === dayjs().format('DD-MM-YY')
-      ? 'bg-blue-600 text-white rounded-full w-7 h-7'
-      : '';
-  };
+  const { setDaySelected, setShowEventModal, savedEvents, setSelectedEvent } =
+    useContext(GlobalContext);
+  const [dayEvents, setDayEvents] = useState([]);
+
+  useEffect(() => {
+    const events = savedEvents.filter(
+      (event) => dayjs(event.day).format('DD-MM-YY') === day.format('DD-MM-YY')
+    );
+    setDayEvents(events);
+  }, [savedEvents, day]);
+
+  const currentClass = getCurrentDayClass(day);
+
   return (
     <div className="border border-gray-200 flex flex-col">
       <header className="flex flex-col items-center">
         {rowIndex % 7 === 0 && (
           <p className="text-sm mt-1">{day.format('ddd').toUpperCase()}</p>
         )}
-        <p className={`text-sm p-1 my-1 text-center ${getCurrentDayClass()}`}>
+        <p className={`text-sm p-1 my-1 text-center ${currentClass}`}>
           {day.format('DD')}
         </p>
       </header>
+      <div
+        className="flex-1 cursor-pointer"
+        onClick={() => {
+          setDaySelected(day);
+          setShowEventModal(true);
+        }}>
+        {dayEvents.map((event, idx) => (
+          <div
+            key={idx}
+            onClick={() => setSelectedEvent(event)}
+            className={`${event.label} p-2 mr-3 text-gray-100 text-sm rounded mb-1 truncate`}>
+            {event.title}
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
